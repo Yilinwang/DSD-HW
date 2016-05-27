d48d09b1d05b936c8dd5189da1546636b639c5e5
chipyaya <b03902024@ntu.edu.tw>
2016-05-27 12:18:35 +0800

    add counter and test_bench

--- lab2/counter.v
+++ lab2/counter.v
@@ -1,1 +1,30 @@
-module counter(
+module counter(enable, reset, clock, counter_o);
+input enable;
+input reset;
+input clock;
+output reg [7:0] counter_o;
+reg [20:0] tmp;
+
+initial begin
+	tmp = 8'b00000000;
+	counter_o = 8'b00000000;	//10^-2
+end
+
+
+always @(posedge clock)
+begin
+	if(reset) begin
+		counter_o = 8'b00000000;
+	end
+	else if(enable) begin
+		tmp = tmp + 1;
+	end
+
+	if(tmp == 500000) begin
+		tmp = 8'b00000000;
+		counter_o = counter_o + 1;
+		$display("%4dns: counter_o=%d", $time, counter_o); 
+	end
+end
+
+endmodule
---
+++ lab2/main.v
@@ -0,0 +1,22 @@
+`include "counter.v"
+module main();
+
+reg enable, reset, clock;
+wire [7:0] counter_o;
+
+counter c0(enable, reset, clock, counter_o);
+
+initial begin
+	clock = 1;
+	reset = 0;  
+	enable = 0;
+	#5 reset = 1;
+	#10 reset = 0;
+	#5 enable = 1;
+end
+
+always #10 begin	//20ns clock: 1->0->1
+	clock=~clock; 
+end
+
+endmodule
