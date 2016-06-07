5d66bf141fb728ce7acea3a72e090d75a7bbe72c
chipyaya <b03902024@ntu.edu.tw>
2016-06-07 15:07:13 +0800

    add not

--- final_forward/public/js/final.js
+++ final_forward/public/js/final.js
@@ -38,7 +38,7 @@ function doCanvas(arg) {
 		var web_h = $(document).height();
 		var web_w = $(document).width();
 		var FF_x = web_w-500;
-		leftLine(parseInt(input_N)+parseInt(FF_N));
+		leftLine(input_N, input_N+FF_N);
 		rightLine(FF_N, input_N);
 		//arg[1] >= 6 will overlap
 		link(1, FF_N, input_N+FF_N, arg[6]);
@@ -55,9 +55,6 @@ function doCanvas(arg) {
 		*/
 		}
 
-		//NOT(550, 450);
-		//OR(550, 600);
-		//AND(550, 750);
 	}
 
 	// Runs each time the DOM window resize event fires.
@@ -72,33 +69,56 @@ function doCanvas(arg) {
 		//J1=120 211 -> y2 * x' + y1 * x
 		var str = data.split(' ');
 		var i, j;
-		var times = 0;
 		var web_h = $(document).height();
 		var web_w = $(document).width();
 		var FF_x = web_w-500;
-		console.log(FF_y);
+		var FF_y = (FF_i/(parseInt(FF_N)+1))*web_h;
+		var and_w = 50;
+		var and_h = 50;
+		var hh = [];
 		for(i = 0; i < str.length; i++){
-			var FF_y = (FF_i/(parseInt(FF_N)+1))*web_h;
+			var times = 0;
+			var move_y = (i == 0)? -100:-40;
 			for(j = 0; j < sum; j++){
 				if(parseInt(str[i][j]) != 2){
 					var move_x = (parseInt(str[i][j]) == 1)? 0:1;
-					var move_y = (i == 0)? -30:0;
+					var unit_l = 20;
 					//console.log('FF_i = '+FF_i+' times = '+times+' i = '+i+' j= '+j);
-					line_xxyy(10+10*((sum-j-1)*2+move_x), FF_y+move_y+10*(times), 200, FF_y+10*(times+2*i));
+					line_xxyy(10+unit_l*((sum-j-1)*2+move_x), FF_y+move_y+unit_l*(times), 200, FF_y+move_y+unit_l*(times));
 					times = times + 1;
 				}
 			}
+			AND(200, FF_y+move_y+unit_l*(times-1)/2-and_h/2, and_w, and_h)
+			hh.push(FF_y+move_y+unit_l*(times-1)/2);
 		}
+		var avg = 0;
+		for(i = 0; i < hh.length; i++){
+			avg += hh[i];
+		}
+		avg = avg / hh.length;
+		var or_w = 80;
+		var or_h = 120;
+		OR(200+2*and_w+0.5*and_h, avg-or_h/2, or_w, or_h);
 	}
 
-	function leftLine(sum){
+	function leftLine(input_N, sum){
 		var web_h = $(document).height();
 		var web_w = $(document).width();
 		var input_x = 10;
-		var unit = 10;
+		var unit = 20;
 		var i;
 		for(i = 1; i <= 2*sum; i++){
-			line_len(input_x+(i-1)*unit, 90, 0, 800);
+			if(i <= input_N){
+				cxt.font = "18px Consola";
+				cxt.fillText('X'+i, input_x+(i-1)*unit-10, 40);
+				line_len(input_x+(i-1)*unit, 90-40, 0, 800+40);
+			}
+			else{
+				line_len(input_x+(i-1)*unit, 90, 0, 800);
+			}
+			if(i % 2 == 1){
+				NOT(input_x+(i-1)*unit, 90-5*2.732, 21);
+			}
 		}
 	}
 
@@ -110,17 +130,18 @@ function doCanvas(arg) {
 		var width = 150;
 		var height = 150;
 		var input_x = 10;
-		var unit = 30;
+		var unit_r = 30;
+		var unit_l = 20;
 
 		var first_y = (1/(parseInt(FF_N)+1))*web_h;
 		for(i = 1; i <= FF_N; i++){
 			var origin_y = (i/(parseInt(FF_N)+1))*web_h;
 			var x = FF_x+0.9*width;
 			var y = origin_y-0.3*height;
-			line_len(x, y, (i-1)*unit, 0);
-			line_xxyy(x+(i-1)*unit, y, x+(i-1)*unit, 30+(FF_N-i)*0.5*unit);
-			line_xxyy(x+(i-1)*unit, 30+(FF_N-i)*0.5*unit, 10+10*(2*input_N+2*(FF_N-i)), 30+(FF_N-i)*0.5*unit);
-			line_xxyy(10+10*(2*input_N+2*(FF_N-i)), 30+(FF_N-i)*0.5*unit, 10+10*(2*input_N+2*(FF_N-i)), 90);
+			line_len(x, y, (i-1)*unit_r, 0);
+			line_xxyy(x+(i-1)*unit_r, y, x+(i-1)*unit_r, 30+(FF_N-i)*0.5*unit_r);
+			line_xxyy(x+(i-1)*unit_r, 30+(FF_N-i)*0.5*unit_r, 10+unit_l*(2*input_N+2*(FF_N-i)), 30+(FF_N-i)*0.5*unit_r);
+			line_xxyy(10+unit_l*(2*input_N+2*(FF_N-i)), 30+(FF_N-i)*0.5*unit_r, 10+unit_l*(2*input_N+2*(FF_N-i)), 90);
 		}
 
 	}
@@ -180,61 +201,34 @@ function doCanvas(arg) {
 	}
 
 	// Gates and FFs
-	function NOT(x, y){
-		var width = 25;
-		var height = 25;
-		cxt.moveTo(x, y);
-		cxt.lineTo(x+1.5*width, y+height);
-		cxt.moveTo(x+1.5*width, y+height);
-		cxt.lineTo(x, y+2*height);
-		cxt.moveTo(x, y+2*height);
-		cxt.lineTo(x, y);
-		cxt.moveTo(x+1.5*width+10, y+height);
-		cxt.arc(x+1.5*width+5,y+height,5,0,2*Math.PI);
-		cxt.moveTo(x+1.5*width+10, y+height);
-		cxt.lineTo(x+1.5*width+10+width, y+height);
-		cxt.moveTo(x, y+height);
-		cxt.lineTo(x-width, y+height);
+	function NOT(x, y, unit){
+		line_len(x, y, 0.7*unit, 0);
+		line_len(x+0.45*unit, y-1.732/4*unit, unit, 0);
+		line_len(x+0.45*unit, y-1.732/4*unit, 0.5*unit, 1.732/2*unit);
+		line_len(x+1.45*unit, y-1.732/4*unit, -0.5*unit, 1.732/2*unit);
+		var radius = unit / 8;
+		cxt.moveTo(x+0.95*unit+radius, y+1.732/4*unit+radius);
+		cxt.arc(x+0.95*unit,y+1.732/4*unit+radius,radius,0,2*Math.PI);
 		cxt.stroke();
 	}
 
-	function AND(x, y){
-		var width = 25;
-		var height = 25;
-		cxt.moveTo(x, y);
-		cxt.lineTo(x+width, y);
+	function AND(x, y, width, height){
+		line_len(x, y, width, 0);
 		cxt.moveTo(x+width, y);
-		cxt.arc(x+width, y+height, height,1.5*Math.PI,0.5*Math.PI);
-		cxt.moveTo(x+width, y+2*height);
-		cxt.lineTo(x, y+2*height);
-		cxt.moveTo(x, y+2*height);
-		cxt.lineTo(x, y);
-		cxt.moveTo(x, y+0.5*height);
-		cxt.lineTo(x-width, y+0.5*height);
-		cxt.moveTo(x, y+1.5*height);
-		cxt.lineTo(x-width, y+1.5*height);
-		cxt.moveTo(x+width+height, y+height);
-		cxt.lineTo(x+width+height+width, y+height);
+		cxt.arc(x+width, y+0.5*height, 0.5*height, 1.5*Math.PI, 0.5*Math.PI);
+		line_len(x, y+height, width, 0);
+		line_len(x, y, 0, height);
+		line_len(x+width+0.5*height, y+0.5*height, 1.5*width, 0);
 		cxt.stroke();
 	}
 
-	function OR(x, y){
-		var width = 25;
-		var height = 25;
-		cxt.moveTo(x, y);
-		cxt.lineTo(x+width, y);
+	function OR(x, y, width, height){
+		line_len(x, y, width, 0);
 		cxt.moveTo(x+width, y);
-		cxt.arc(x+width, y+height, height,1.5*Math.PI,0.5*Math.PI);
-		cxt.moveTo(x+width, y+2*height);
-		cxt.lineTo(x, y+2*height);
+		cxt.arc(x+width, y+0.5*height, 0.5*height,1.5*Math.PI,0.5*Math.PI);
 		cxt.moveTo(x, y);
-		cxt.quadraticCurveTo(x+width, y+height, x, y+2*height);
-		cxt.moveTo(x+width+height, y+height);
-		cxt.lineTo(x+width+height+width, y+height);
-		cxt.moveTo(x+0.4*width, y+0.6*height);
-		cxt.lineTo(x-width, y+0.6*height);
-		cxt.moveTo(x+0.4*width, y+1.4*height);
-		cxt.lineTo(x-width, y+1.4*height);
+		cxt.quadraticCurveTo(x+width, y+0.5*height, x, y+height);
+		line_len(x, y+height, width, 0);
 		cxt.stroke();
 	}
 
